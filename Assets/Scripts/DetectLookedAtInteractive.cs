11b531c77740d025771944b7a5afd7079205be89
eolsonp <eolsonp@gmail.com>
2019-04-04 17:46:36 -0500

    add interactive script
    interacive scripts

---
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Interactable object deterction system
+/// </summary>
+public class DetectLookedAtInteractive : MonoBehaviour
+{
+    [Tooltip("Starting point of raycast used to detect interactives.")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("How far from the raycastOrigin we will search for interactive elements")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxRange, Color.red);
+        RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+       
+        if (objectWasDetected)
+        {
+            Debug.Log($"You are looking at:  {hitInfo.collider.gameObject.name}");
+        }
+
+    }
+}
