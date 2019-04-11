595770b4cf4712baa1b1fe18aaf7f1323974bb62
eolsonp <eolsonp@gmail.com>
2019-04-11 16:33:20 -0500

    create interface scripts
    interactive, IInteractive, Detect, InteractWith

--- Assets/Scripts/DetectLookedAtInteractive.cs
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -20,11 +20,17 @@ public class DetectLookedAtInteractive : MonoBehaviour
         Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxRange, Color.red);
         RaycastHit hitInfo;
         bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+
+        IInteractive interactive = null;
        
         if (objectWasDetected)
         {
-            Debug.Log($"You are looking at:  {hitInfo.collider.gameObject.name}");
+            //Debug.Log($"You are looking at:  {hitInfo.collider.gameObject.name}");
+            interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
         }
+
+        if (interactive != null)
+            interactive.InteractWith(); 
 
     }
 }
