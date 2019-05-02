37289c6373d53892fbc81e4f337cc05279e0f9fd
eolsonp <eolsonp@gmail.com>
2019-05-02 04:51:27 -0500

    door works

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : InteractiveObject
+{
+    private Animator animator;
+    private bool isOpen = false;
+
+    /// <summary>
+    /// Using a constructor here to initialize displayText in the editor
+    /// </summary>
+    public Door()
+    {
+        displayText = nameof(Door);
+    }
+
+    protected override void Awake()
+    {
+        {
+            base.Awake();
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    public override void InteractWith()
+    {
+        if(!isOpen)
+        {
+            base.InteractWith();
+            animator.SetBool("shouldOpen", true);
+            displayText = string.Empty;
+            isOpen = true;
+        }
+    }
+}
