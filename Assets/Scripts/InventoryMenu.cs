959602dd4ef3b39e5b081ce644d9e5635bdaea65
eolsonp <eolsonp@gmail.com>
2019-05-06 03:16:46 -0500

    work on menu

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+    private CanvasGroup canvasGroup;
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance == null)
+                throw new System.Exception("There is currently no InventoryMenu instance, " + "but you are trying to access it! MAke sure the InventoryMenu script is applied to a GameObject in your scene!");
+            return instance;
+        }
+
+        private set { instance = value; }
+    }
+
+    private bool IsVisible => canvasGroup.alpha > 0;
+
+    private void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+    }
+
+    private void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+    }
+
+    private void Update()
+    {
+        {
+            HandleInput();
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Show Inventory Menu"))
+            if (IsVisible)
+                HideMenu();
+            else
+                ShowMenu();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            throw new System.Exception("There is already an instance of InventoryMenu and there can only be one.");
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        HideMenu();
+    }
+}
