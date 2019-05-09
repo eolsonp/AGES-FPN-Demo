269b744964bcb30ea23ffdf83d42dca60e13ee13
eolsonp <eolsonp@gmail.com>
2019-05-08 21:10:13 -0500

    added flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light flashLight;
+    private bool lightOn = false;
+   
+        void Start()
+        {
+            flashLight = GetComponentInChildren<Light>();
+        }
+
+        void Update()
+        {
+            if (lightOn)
+                flashLight.enabled = true;
+            else if (!lightOn)
+                flashLight.enabled = false;
+            if (Input.GetKeyDown(KeyCode.F))
+                lightOn = !lightOn;
+        }
+    }
